1ce80605d83d01d9ac198045c201f146cf15d569
Izalimoli <izalimoli@hotmail.com>
2025-06-14 15:19:18 -0400

    dia 2 - primeiro teste

---
+++ dia2.R
@@ -0,0 +1,10 @@
+# Exemplo simples em R: média de notas
+
+# Vetor com notas de alunos
+notas <- c(8.5, 7.0, 9.2, 6.8, 10)
+
+# Calculando a média
+media <- mean(notas)
+
+# Imprimindo o resultado
+cat("A média das notas é:", media, "\n")
